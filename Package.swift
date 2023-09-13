2b604d75bb4e2d16ab07ad70383aabfc8061a257
Sullied Security <sulliedeclat@gmail.com>
2023-09-13 12:41:56 -0400

    Updated measurement dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git://kubernetes.convex.watsonkp.com:31950/SulliedMeasurement", from: "0.3.1")
+        .package(url: "git://kubernetes.convex.watsonkp.com:31950/SulliedMeasurement", from: "0.3.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
