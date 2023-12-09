b5c6731c002ee135e3dc09f36155d739a4730813
Sullied Security <sulliedeclat@gmail.com>
2023-12-09 12:13:27 -0500

    Debugging design time measurement publishing

--- Sources/SulliedBluetooth/Preview Content/DesignTimePeripheralController.swift
+++ Sources/SulliedBluetooth/Preview Content/DesignTimePeripheralController.swift
@@ -14,14 +14,6 @@ class DesignTimePeripheralController: PeripheralControllerProtocol {
     init(model: PeripheralModel) {
         self.model = model
         let start = Date.now
-        // Update heart rate characteristic values
-        Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect().map { _ in
-            var data = Data(count: 2)
-            data[1] = 123
-            data[1] = UInt8(170 + 10 * sin(Date.now.timeIntervalSince(start) * Double.pi / 8))
-            return data
-        }
-        .assign(to: &model.services.first(where: { $0.uuid == CBUUID(string: "0x180d") })!.characteristics.first!.$value)
 
         // Publish heart rate characteristic values
         Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect().map { _ in
@@ -34,5 +26,14 @@ class DesignTimePeripheralController: PeripheralControllerProtocol {
                                                        significantFigures: 3,
                                                        significantPosition: 0))
         })
+
+        // Update heart rate characteristic values
+        Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect().map { _ in
+            var data = Data(count: 2)
+            data[1] = 123
+            data[1] = UInt8(170 + 10 * sin(Date.now.timeIntervalSince(start) * Double.pi / 8))
+            return data
+        }
+        .assign(to: &model.services.first(where: { $0.uuid == CBUUID(string: "0x180d") })!.characteristics.first!.$value)
     }
 }
