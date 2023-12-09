8b1a643c344bd99523b8b3b5ec127fb88ac399f7
Sullied Security <sulliedeclat@gmail.com>
2023-12-09 18:39:22 -0500

    Debugging design time publishing with simplified implementation

--- Sources/SulliedBluetooth/Preview Content/DesignTimePeripheralController.swift
+++ Sources/SulliedBluetooth/Preview Content/DesignTimePeripheralController.swift
@@ -10,22 +10,35 @@ class DesignTimePeripheralController: PeripheralControllerProtocol {
         }
     }
     var recordPublisher = PassthroughSubject<IntegerDataPoint, Never>()
+    private var subscriptions: [AnyCancellable] = []
 
     init(model: PeripheralModel) {
         self.model = model
         let start = Date.now
 
         // Publish heart rate characteristic values
-        Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect().map { _ in
-            Int64(model.services.first(where: { $0.uuid == CBUUID(string: "0x180d") })!.characteristics.first!.parsedValue.description)!
-        }.sink(receiveValue: {
-            self.recordPublisher.send(IntegerDataPoint(date: Date.now,
-                                                       unit: UnitFrequency.beatsPerMinute,
-                                                       usage: .heartRate,
-                                                       value: $0,
-                                                       significantFigures: 3,
-                                                       significantPosition: 0))
-        })
+        Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect()
+            .map {
+                let value = Int64($0.timeIntervalSince(start))
+                return IntegerDataPoint(date: $0,
+                                        unit: UnitFrequency.beatsPerMinute,
+                                        usage: .heartRate,
+                                        value: value,
+                                        significantFigures: 3,
+                                        significantPosition: 0)
+            }
+            .sink(receiveValue: { self.recordPublisher.send($0) })
+            .store(in: &subscriptions)
+//        Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect().map { _ in
+//            Int64(model.services.first(where: { $0.uuid == CBUUID(string: "0x180d") })!.characteristics.first!.parsedValue.description)!
+//        }.sink(receiveValue: {
+//            self.recordPublisher.send(IntegerDataPoint(date: Date.now,
+//                                                       unit: UnitFrequency.beatsPerMinute,
+//                                                       usage: .heartRate,
+//                                                       value: $0,
+//                                                       significantFigures: 3,
+//                                                       significantPosition: 0))
+//        })
 
         // Update heart rate characteristic values
         Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect().map { _ in
