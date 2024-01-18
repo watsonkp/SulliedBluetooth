35d0ed3ce96af25124955b0e50ddc28b0bcd42fe
Sullied Security <sulliedeclat@gmail.com>
2024-01-18 14:17:17 -0500

    Added UUID to description look-up for services and characteristics
    Changed service and characteristic models to use the new look-up dictionary.

--- Sources/SulliedBluetooth/Model/CharacteristicModel.swift
+++ Sources/SulliedBluetooth/Model/CharacteristicModel.swift
@@ -16,46 +16,9 @@ class CharacteristicModel: ObservableObject {
     }
     @Published var value: Data? = nil
     @Published var isNotifying: Bool = false
-    private static let characteristicNames: [CBUUID: String] = [CBUUID(string: "0x2a19"): "Battery Level",
-                                                                CBUUID(string: "0x2a23"): "System ID",
-                                                                CBUUID(string: "0x2a24"): "Model Number String",
-                                                                CBUUID(string: "0x2a25"): "Serial Number String",
-                                                                CBUUID(string: "0x2a26"): "Firmware Revision String",
-                                                                CBUUID(string: "0x2a27"): "Hardware Revision String",
-                                                                CBUUID(string: "0x2a28"): "Software Revision String",
-                                                                CBUUID(string: "0x2a29"): "Manufacturer Name String",
-                                                                CBUUID(string: "0x2a37"): "Heart Rate Measurement",
-                                                                CBUUID(string: "0x2a38"): "Body Location",
-                                                                CBUUID(string: "0x2a5b"): "CSC Measurement",
-                                                                CBUUID(string: "0x2a5d"): "Sensor Location",
-                                                                CBUUID(string: "0x2a63"): "Cycling Power Measurement",
-                                                                CBUUID(string: "0x2A65"): "Cycling Power Feature",
-                                                                CBUUID(string: "0x2A66"): "Cycling Power Control Point",
-                                                                CBUUID(string: "0x2a80"): "Age",
-                                                                CBUUID(string: "0x2a8a"): "First Name",
-                                                                CBUUID(string: "0x2a8c"): "Gender",
-                                                                CBUUID(string: "0x2a8e"): "Height",
-                                                                CBUUID(string: "0x2a90"): "Last Name",
-                                                                CBUUID(string: "0x2a98"): "Weight",
-                                                                CBUUID(string: "0x2a99"): "Database Change Increment",
-                                                                CBUUID(string: "0x2a9a"): "User Index",
-                                                                CBUUID(string: "0x2a9f"): "User Control Point",
-                                                                CBUUID(string: "0x2AA2"): "Language",
-                                                                CBUUID(string: "0x2ACC"): "Fitness Machine Feature",
-                                                                CBUUID(string: "0x2AD2"): "Indoor Bike Data",
-                                                                CBUUID(string: "0x2AD6"): "Supported Resistance Level Range",
-                                                                CBUUID(string: "0x2AD8"): "Supported Power Range",
-                                                                CBUUID(string: "0x2AD9"): "Fitness Machine Control Point",
-                                                                CBUUID(string: "0x2ADA"): "Fitness Machine Status"]
 
     var name: String {
-        get {
-            if let name = CharacteristicModel.characteristicNames[self.uuid] {
-                return name
-            } else {
-                return self.uuid.uuidString
-            }
-        }
+        CharacteristicUUIDDescriptions[self.uuid] ?? self.uuid.uuidString
     }
 
     var parsedValue: BluetoothValue {
--- Sources/SulliedBluetooth/Model/ServiceModel.swift
+++ Sources/SulliedBluetooth/Model/ServiceModel.swift
@@ -3,20 +3,9 @@ import CoreBluetooth
 class ServiceModel: ObservableObject {
     @Published var characteristics = [CharacteristicModel]()
     var uuid: CBUUID
-    private static let serviceNames: [CBUUID: String] = [CBUUID(string: "0x180d"): "Heart Rate",
-                                                         CBUUID(string: "0x180a"): "Device Information",
-                                                         CBUUID(string: "0x180f"): "Battery",
-                                                         CBUUID(string: "0x1816"): String(describing: CBUUID(string: "0x1816")),
-                                                         CBUUID(string: "0x1818"): "Cycling Power",
-                                                         CBUUID(string: "0x181c"): "User Data",
-                                                         CBUUID(string: "0x1826"): "Fitness Machine"]
 
     var name: String {
-        if let name = ServiceModel.serviceNames[self.uuid] {
-            return name
-        } else {
-            return self.uuid.uuidString
-        }
+        ServiceUUIDDescriptions[self.uuid] ?? self.uuid.uuidString
     }
     
     init(_ service: CBService) {
