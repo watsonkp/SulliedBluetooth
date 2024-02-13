ea7c3afbe1b36c09bef073670cb06abbf24b442b
Sullied Security <sulliedeclat@gmail.com>
2024-02-13 16:01:51 -0500

    Removed reliance on Apple's implemented descriptions for development and localization

--- Sources/SulliedBluetooth/Bluetooth.swift
+++ Sources/SulliedBluetooth/Bluetooth.swift
@@ -36,10 +36,10 @@ public struct Bluetooth {
 
     // YAML reference of service UUIDs
     //  https://bitbucket.org/bluetooth-SIG/public/src/main/assigned_numbers/uuids/service_uuids.yaml
-    static let supportedServices = [SupportedService(id: CBUUID(string: "0x180d"), description: String(describing: CBUUID(string: "0x180d"))),
-                                    SupportedService(id: CBUUID(string: "0x180a"), description: String(describing: CBUUID(string: "0x180a"))),
-                                    SupportedService(id: CBUUID(string: "0x180f"), description: String(describing: CBUUID(string: "0x180f"))),
-                                    SupportedService(id: CBUUID(string: "0x1816"), description: String(describing: CBUUID(string: "0x1816"))),
+    static let supportedServices = [SupportedService(id: CBUUID(string: "0x180d"), description: "Heart Rate"),
+                                    SupportedService(id: CBUUID(string: "0x180a"), description: "Device Information"),
+                                    SupportedService(id: CBUUID(string: "0x180f"), description: "Battery"),
+                                    SupportedService(id: CBUUID(string: "0x1816"), description: "Cycling Speed and Cadence"),
                                     SupportedService(id: CBUUID(string: "0x1818"), description: "Cycling Power"),
                                     SupportedService(id: CBUUID(string: "0x181c"), description: "User Data"),
                                     SupportedService(id: CBUUID(string: "0x1826"), description: "Fitness Machine")]
